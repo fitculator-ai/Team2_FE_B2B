dd1ba27ffb656dc695a8ffa529eb940a274558d6
kimHyeonJu <kimhyesom1202@gmail.com>
2025-02-14 15:03:31 +0900

    [생성] swiper 생성 및 적용(예시)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import { Metadata } from "next";
+import Swiper from "@/components/Swiper/Swiper";
+
+export const metadata: Metadata = {
+  title: "Fitculator",
+  description: "운동에 진심인 당신을 위한 운동량 계산기",
+};
+
 export default function RootPage() {
-  return <h1 className="text-xl font-bold">Root Page!</h1>;
+  return (
+    <>
+      <h1 className="text-xl font-bold underline"> Root Page!</h1>
+      <Swiper />
+    </>
+  );
 }
