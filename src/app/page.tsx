83d8f61f0a9b8de4bda4b04bdd76069dc98bddd1
2yerin <lyerin0113@naver.com>
2025-02-26 11:30:14 +0900

    [수정/생성] sectionLayout props 수정, 섹션 컴포넌트 추가(통계, 팀 정보)
    - sectionLayout 컴포넌트 props 수정
    - 통계, 팀 정보 섹션 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-// import SectionLayout from "@/components/Layout/SectionLayout";
+import StatisticsSection from "@/components/StatisticsSection";
+import TeamInfoSection from "@/components/TeamInfoSection";
+import ProcessSection from "@/components/ProcessSection";
+import ContactUsSection from "@/components/ContactUsSection";
+import FAQSection from "@/components/FAQSection";
 
 export default function RootPage() {
   return (
     <>
-      {/* <SectionLayout
-        isFullPage="true"
-        title="partnership Review"
-        subTitle="Fit만의 솔루션"
-        position="center"
-        subTitleColor="point"
-        description="수많은 기업에서 이미 사용하고 있어요요"
-      >
-        <h1>하위 내용</h1>
-      </SectionLayout> */}
+      {/* <h1 className="text-xl font-bold underline"> Root Page!</h1> */}
+      <StatisticsSection />
+      <TeamInfoSection />
+      <ProcessSection />
+      <FAQSection />
+      <ContactUsSection />
     </>
   );
 }
