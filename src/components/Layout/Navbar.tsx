07b12e098507c5a912bed9808d1530c40138e882
2yerin <lyerin0113@naver.com>
2025-02-26 15:58:50 +0900

    [수정] NavBar 로고 이미지 추가

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+import mainLogo from "../../../public/image/logo_text.png";
+
 export default function NavBar() {
   return (
     <nav className="fixed top-0 left-0 flex w-full justify-between bg-white p-4">
-      <button>로고</button>
-      <ul className="flex gap-4">
+      <button>
+        <Image
+          src={mainLogo}
+          className="h-[1.5625rem] w-[10.125rem]"
+          width={162}
+          height={25}
+          alt="fitculator 로고"
+          aria-label="핏큘레이터"
+        />
+      </button>
+
+      <ul className="flex gap-8">
         <li>
           <a href="\">스튜디오 제휴</a>
         </li>
