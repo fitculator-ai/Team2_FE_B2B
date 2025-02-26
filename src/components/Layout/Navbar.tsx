6dd45b03ce16a4f972824e898c47d2f7099cb3f6
2yerin <lyerin0113@naver.com>
2025-02-27 01:12:25 +0900

    [수정] 레이아웃 반응형 스타일 수정, 아이콘 추가
    - 레이아웃 반응형 스타일 수정
    - footer 아이콘, 로고 이미지 추가

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -4,17 +4,17 @@ import mainLogo from "../../../public/image/logo_text.png";
 
 export default function NavBar() {
   return (
-    <nav className="fixed top-0 left-0 flex w-full justify-between bg-white px-6 py-5 sm:px-10 md:px-12 lg:px-18">
+    <nav className="fixed top-0 left-0 flex w-full justify-between bg-[#F9F9F9] px-7 py-5 md:px-15">
       <Link href="/#home">
         <Image
           src={mainLogo}
-          className="h-[1.5rem] w-[9.625rem] md:h-[1.5625rem] md:w-[10.125rem]"
+          className="h-[1.5625rem] w-[10.125rem]"
           alt="fitculator 로고"
           aria-label="핏큘레이터"
         />
       </Link>
 
-      <ul className="flex gap-5 font-medium sm:gap-6 md:gap-10">
+      <ul className="flex gap-5 font-medium md:gap-10">
         <li>
           <a href="\">스튜디오 제휴</a>
         </li>
