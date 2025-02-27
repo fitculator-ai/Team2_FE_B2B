e325d149bd57be91f6ad9cad1d2e89312b18d70d
2yerin <lyerin0113@naver.com>
2025-02-28 01:19:59 +0900

    [생성] 메인 섹션 파일 생성

---
+++ src/sections/MainSection.tsx
@@ -0,0 +1,9 @@
+import SectionLayout from "@/components/Layout/SectionLayout";
+
+export default function MainSection() {
+  return (
+    <SectionLayout>
+      <h1>메인 섹션</h1>
+    </SectionLayout>
+  );
+}
