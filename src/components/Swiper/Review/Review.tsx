8ad0fb989b55c16437a98b50b3ec0b55773881fc
JubiJung <devvjubi@gmail.com>
2025-02-28 01:37:06 +0000

    [기능] Swiper 개선 - 데이터 개선 관리 → 상수 명명 규칙적용

--- src/components/Swiper/Review/Review.tsx
+++ src/components/Swiper/Review/Review.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 interface reviewType {
   name: string;
   rank: string;
@@ -5,13 +7,17 @@ interface reviewType {
 }
 
 export default function Review({ name, rank, desc }: reviewType) {
-  return (
-    <div className="bg-opacity-68 flex h-full min-h-[22.5rem] flex-col gap-[2.25rem] bg-[#F2F6F8] p-[1.25rem]">
-      <div className="flex flex-col gap-[0.25rem] text-[#4FD5FF]">
-        <h3 className="text-[1.25rem] font-bold">{name}님</h3>
-        <span className="text-[1rem] font-light">{rank}</span>
+  const reviewContent = useMemo(() => {
+    return (
+      <div className="bg-opacity-68 flex h-full min-h-[22.5rem] flex-col gap-[2.25rem] bg-[#F2F6F8] p-[1.25rem]">
+        <div className="flex flex-col gap-[0.25rem] text-[#4FD5FF]">
+          <h3 className="text-[1.25rem] font-bold">{name}님</h3>
+          <span className="text-[1rem] font-light">{rank}</span>
+        </div>
+        <p className="text-[1.125rem] text-[#000]">{desc}</p>
       </div>
-      <p className="text-[1.125rem] text-[#000]">{desc}</p>
-    </div>
-  );
+    );
+  }, [name, rank, desc]);
+
+  return reviewContent;
 }
